6b7998cd9fabab436ef1b76445a43e90e415d90a
Bekar-T <b.tchaphodze@gmail.com>
2024-01-26 20:29:09 +0400

    Completed all tasks

--- homework3/script.js
+++ homework3/script.js
@@ -2,13 +2,42 @@
 
 let number = 5;
 
-let square = number ** 2;
+console.log(number ** 2);
 
 // 2)
 
 let celsius = 12;
 
-let fahrenheit = (celsius * 9/5) + 32;
+console.log((celsius * 9 / 5) + 32);
 
 // 3)
 
+let start = 3;
+let end = 18;
+let given = 9;
+
+console.log(start <= given && given <= end);
+
+// 4)
+
+year = 2024;
+
+console.log(year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+// 5)
+
+let num1 = Number(prompt("შეიყვანეთ პირველი რიცხვი:"));
+let num2 = Number(prompt("შეიყვანეთ მეორე როცხვი:"));
+let action = prompt("შეიყვანეთ ერთერთი მოქმედება (+, -, *, /):");
+
+if (action === "+") {
+    alert(`თქვენი პასუხი არის ${num1+num2}.`);
+} else if (action === "-") {
+    alert(`თქვენი პასუხი არის ${num1-num2}.`);
+} else if (action === "*") {
+    alert(`თქვენი პასუხი არის ${num1*num2}.`);
+} else if (action === "/") {
+    alert(`თქვენი პასუხი არის ${num1/num2}.`);
+} else {
+    alert("დაფიქსირდა არასწორი მონაცემი.\n\nგთხოვთ სცადოთ თავიდან.");
+};
