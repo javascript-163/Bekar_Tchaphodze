f717e6507840a9724eaa2c414b4047218e091f0b
Bekar-T <b.tchaphodze@gmail.com>
2024-03-14 13:28:03 +0400

    Forms mostly done

--- AoDI_JS_Final/js/scripts.js
+++ AoDI_JS_Final/js/scripts.js
@@ -1,17 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuButton = document.getElementById('menu_button');
-    const icon = menuButton.querySelector('i');
-    const nav = document.querySelector("nav");
-  
-    menuButton.addEventListener("click", () => {
-      if (nav.style.display === "block") {
-        nav.style.display = "none";
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
-      } else {
-        nav.style.display = "block";
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-xmark');
-      }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Burger menu functionality
+
+  const menuButton = document.getElementById('menu_button');
+  const icon = menuButton.querySelector('i');
+  const nav = document.querySelector("nav");
+
+  menuButton.addEventListener("click", () => {
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+      icon.classList.remove('fa-xmark');
+      icon.classList.add('fa-bars');
+    } else {
+      nav.style.display = "block";
+      icon.classList.remove('fa-bars');
+      icon.classList.add('fa-xmark');
+    }
+  });
+
+
+  // Auth menu functionality
+
+  const userButton = document.getElementById("user_button");
+  const userMenu = document.getElementById("user_menu");
+
+  userButton.addEventListener("click", () => {
+    if (userMenu.style.display === "flex") {
+      userMenu.style.display = "none";
+    } else {
+      userMenu.style.display = "flex";
+    }
+  });
+
 });
