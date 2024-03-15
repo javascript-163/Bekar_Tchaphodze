888218134aabb94acfeb673ebfe0823c3efb63eb
Bekar-T <b.tchaphodze@gmail.com>
2024-03-15 19:45:40 +0400

    Finished all the way

--- AoDI_JS_Final/js/scripts.js
+++ AoDI_JS_Final/js/scripts.js
@@ -46,6 +46,41 @@ document.addEventListener("DOMContentLoaded", function () {
     ["Afghanistan", "AFG"], ["Albania", "ALB"], ["Algeria", "DZA"], ["Andorra", "AND"], ["Angola", "AGO"], ["Antigua and Barbuda", "ATG"], ["Argentina", "ARG"], ["Armenia", "ARM"], ["Australia", "AUS"], ["Austria", "AUT"], ["Azerbaijan", "AZE"], ["Bahamas", "BHS"], ["Bahrain", "BHR"], ["Bangladesh", "BGD"], ["Barbados", "BRB"], ["Belarus", "BLR"], ["Belgium", "BEL"], ["Belize", "BLZ"], ["Benin", "BEN"], ["Bhutan", "BTN"], ["Bolivia", "BOL"], ["Bosnia and Herzegovina", "BIH"], ["Botswana", "BWA"], ["Brazil", "BRA"], ["Brunei", "BRN"], ["Bulgaria", "BGR"], ["Burkina Faso", "BFA"], ["Burundi", "BDI"], ["Cabo Verde", "CPV"], ["Cambodia", "KHM"], ["Cameroon", "CMR"], ["Canada", "CAN"], ["Central African Republic", "CAF"], ["Chad", "TCD"], ["Chile", "CHL"], ["China", "CHN"], ["Colombia", "COL"], ["Comoros", "COM"], ["Congo", "COG"], ["Costa Rica", "CRI"], ["Croatia", "HRV"], ["Cuba", "CUB"], ["Cyprus", "CYP"], ["Czechia", "CZE"], ["Denmark", "DNK"], ["Djibouti", "DJI"], ["Dominica", "DMA"], ["Dominican Republic", "DOM"], ["Ecuador", "ECU"], ["Egypt", "EGY"], ["El Salvador", "SLV"], ["Equatorial Guinea", "GNQ"], ["Eritrea", "ERI"], ["Estonia", "EST"], ["Eswatini", "SWZ"], ["Ethiopia", "ETH"], ["Fiji", "FJI"], ["Finland", "FIN"], ["France", "FRA"], ["Gabon", "GAB"], ["Gambia", "GMB"], ["Georgia", "GEO"], ["Germany", "DEU"], ["Ghana", "GHA"], ["Greece", "GRC"], ["Grenada", "GRD"], ["Guatemala", "GTM"], ["Guinea", "GIN"], ["Guinea-Bissau", "GNB"], ["Guyana", "GUY"], ["Haiti", "HTI"], ["Honduras", "HND"], ["Hungary", "HUN"], ["Iceland", "ISL"], ["India", "IND"], ["Indonesia", "IDN"], ["Iran", "IRN"], ["Iraq", "IRQ"], ["Ireland", "IRL"], ["Israel", "ISR"], ["Italy", "ITA"], ["Jamaica", "JAM"], ["Japan", "JPN"], ["Jordan", "JOR"], ["Kazakhstan", "KAZ"], ["Kenya", "KEN"], ["Kiribati", "KIR"], ["Kosovo", "XKX"], ["Kuwait", "KWT"], ["Kyrgyzstan", "KGZ"], ["Laos", "LAO"], ["Latvia", "LVA"], ["Lebanon", "LBN"], ["Lesotho", "LSO"], ["Liberia", "LBR"], ["Libya", "LBY"], ["Liechtenstein", "LIE"], ["Lithuania", "LTU"], ["Luxembourg", "LUX"], ["Madagascar", "MDG"], ["Malawi", "MWI"], ["Malaysia", "MYS"], ["Maldives", "MDV"], ["Mali", "MLI"], ["Malta", "MLT"], ["Marshall Islands", "MHL"], ["Mauritania", "MRT"], ["Mauritius", "MUS"], ["Mexico", "MEX"], ["Micronesia", "FSM"], ["Moldova", "MDA"], ["Monaco", "MCO"], ["Mongolia", "MNG"], ["Montenegro", "MNE"], ["Morocco", "MAR"], ["Mozambique", "MOZ"], ["Myanmar", "MMR"], ["Namibia", "NAM"], ["Nauru", "NRU"], ["Nepal", "NPL"], ["Netherlands", "NLD"], ["New Zealand", "NZL"], ["Nicaragua", "NIC"], ["Niger", "NER"], ["Nigeria", "NGA"], ["North Korea", "PRK"], ["North Macedonia", "MKD"], ["Norway", "NOR"], ["Oman", "OMN"], ["Pakistan", "PAK"], ["Palau", "PLW"], ["Palestine", "PSE"], ["Panama", "PAN"], ["Papua New Guinea", "PNG"], ["Paraguay", "PRY"], ["Peru", "PER"], ["Philippines", "PHL"], ["Poland", "POL"], ["Portugal", "PRT"], ["Qatar", "QAT"], ["Romania", "ROU"], ["Russia", "RUS"], ["Rwanda", "RWA"], ["Saint Kitts and Nevis", "KNA"], ["Saint Lucia", "LCA"], ["Saint Vincent and the Grenadines", "VCT"], ["Samoa", "WSM"], ["San Marino", "SMR"], ["Sao Tome and Principe", "STP"], ["Saudi Arabia", "SAU"], ["Senegal", "SEN"], ["Serbia", "SRB"], ["Seychelles", "SYC"], ["Sierra Leone", "SLE"], ["Singapore", "SGP"], ["Slovakia", "SVK"], ["Slovenia", "SVN"], ["Solomon Islands", "SLB"], ["Somalia", "SOM"], ["South Africa", "ZAF"], ["South Korea", "KOR"], ["South Sudan", "SSD"], ["Spain", "ESP"], ["Sri Lanka", "LKA"], ["Sudan", "SDN"], ["Suriname", "SUR"], ["Sweden", "SWE"], ["Switzerland", "CHE"], ["Syria", "SYR"], ["Taiwan", "TWN"], ["Tajikistan", "TJK"], ["Tanzania", "TZA"], ["Thailand", "THA"], ["Timor-Leste", "TLS"], ["Togo", "TGO"], ["Tonga", "TON"], ["Trinidad and Tobago", "TTO"], ["Tunisia", "TUN"], ["Turkey", "TUR"], ["Turkmenistan", "TKM"], ["Tuvalu", "TUV"], ["Uganda", "UGA"], ["Ukraine", "UKR"], ["United Arab Emirates", "ARE"], ["United Kingdom", "GBR"], ["United States", "USA"], ["Uruguay", "URY"], ["Uzbekistan", "UZB"], ["Vanuatu", "VUT"], ["Vatican City", "VAT"], ["Venezuela", "VEN"], ["Vietnam", "VNM"], ["Yemen", "YEM"], ["Zambia", "ZMB"], ["Zimbabwe", "ZWE"]
   ];
 
+  class Event {
+    constructor(name, venue, date, url) {
+      this.name = name;
+      this.venue = venue;
+      this.date = date;
+      this.url = url;
+    }
+
+    // method for creating divs for event
+    createEventElement() {
+      const eventDiv = document.createElement('div');
+      eventDiv.classList.add('event');
+
+      const eventName = document.createElement('h3');
+      eventName.textContent = this.name;
+      eventDiv.appendChild(eventName);
+
+      const eventVenue = document.createElement('p');
+      eventVenue.textContent = `Venue: ${this.venue}`;
+      eventDiv.appendChild(eventVenue);
+
+      const eventDate = document.createElement('p');
+      eventDate.textContent = `Date: ${new Date(this.date).toLocaleDateString()}`;
+      eventDiv.appendChild(eventDate);
+
+      const eventLink = document.createElement('a');
+      eventLink.textContent = 'More Info';
+      eventLink.href = this.url;
+      eventLink.target = '_blank';
+      eventDiv.appendChild(eventLink);
+
+      return eventDiv;
+    }
+  }
+
   const inputField = document.getElementById('autocomplete');
   const autocompleteList = document.getElementById('autocomplete-list');
   const widgetInnerRight = document.getElementById('widget_inner_right');
@@ -75,29 +110,12 @@ document.addEventListener("DOMContentLoaded", function () {
           .then(data => {
             widgetInnerRight.innerHTML = '';
 
-            data.events.forEach(event => {
-              const eventDiv = document.createElement('div');
-              eventDiv.classList.add('event');
-
-              const eventName = document.createElement('h3');
-              eventName.textContent = event.name;
-              eventDiv.appendChild(eventName);
-
-              const eventVenue = document.createElement('p');
-              eventVenue.textContent = `Venue: ${event.location.name}, ${event.location.address.addressLocality}, ${event.location.address.addressCountry.name}`;
-              eventDiv.appendChild(eventVenue);
-
-              const eventDate = document.createElement('p');
-              eventDate.textContent = `Date: ${new Date(event.startDate).toLocaleDateString()}`;
-              eventDiv.appendChild(eventDate);
-
-              const eventLink = document.createElement('a');
-              eventLink.textContent = 'More Info';
-              eventLink.href = event.url;
-              eventLink.target = '_blank';
-              eventDiv.appendChild(eventLink);
-              
-              widgetInnerRight.appendChild(eventDiv);
+            data.events.forEach(eventData => {
+              const newEvent = new Event(eventData.name,
+                `${eventData.location.name}, ${eventData.location.address.addressLocality}, ${eventData.location.address.addressCountry.name}`,
+                eventData.startDate, eventData.url);
+                
+              widgetInnerRight.appendChild(newEvent.createEventElement());
             });
           })
           .catch(error => {
